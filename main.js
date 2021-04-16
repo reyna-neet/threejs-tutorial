b2dd6fb71fb743c3ebdf5c33d5f5be4854d6c0a7
Reyna Neet <reyna.neet@gmail.com>
2021-04-16 18:02:31 -0500

    Chapter 2 + switched to Node import

--- main.js
+++
@@ -1,8 +0,0 @@
-import {
-  Camera,
-  Group,
-  Scene,
-} from 'https://unpkg.com/three@0.117.0/build/three.module.js';
-
-import { OrbitControls } from 'https://unpkg.com/three@0.117.0/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'https://unpkg.com/three@0.117.0/examples/jsm/loaders/GLTFLoader.js';
