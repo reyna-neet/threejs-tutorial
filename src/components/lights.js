12a5bdf1b6b37b431d225179c1df4c079fdd554a
Reyna Neet <reyna.neet@gmail.com>
2021-04-18 14:48:20 -0500

    Chapter 4 Medium Challenge 1

--- src/components/lights.js
+++ src/components/lights.js
@@ -1,8 +1,8 @@
-import { DirectionalLight } from 'https://unpkg.com/three@0.127.0/build/three.module.js';
+import { SpotLight } from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 
 function createLights() {
-  const light = new DirectionalLight('skyblue', 8);
-  light.position.set(-15, 25, 0); //meters
+  const light = new SpotLight('skyblue', 10);
+  light.position.set(1, -2, 8); //meters
 
   return light;
 }
