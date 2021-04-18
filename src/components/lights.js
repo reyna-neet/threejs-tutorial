2cba1706429b487fd63cd6299dc86e41680c08c1
Reyna Neet <reyna.neet@gmail.com>
2021-04-18 14:19:07 -0500

    Chapter 4 Easy Challenge 2

--- src/components/lights.js
+++ src/components/lights.js
@@ -1,7 +1,7 @@
 import { DirectionalLight } from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 
 function createLights() {
-  const light = new DirectionalLight('white', 8);
+  const light = new DirectionalLight('skyblue', 8);
   light.position.set(10, 10, 10); //meters
 
   return light;
