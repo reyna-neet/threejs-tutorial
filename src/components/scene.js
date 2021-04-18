d1820194b1d81b83222d74ff19c7182227c7bebd
Reyna Neet <reyna.neet@gmail.com>
2021-04-17 21:18:56 -0500

    Chapter 3 Easy Challenge

--- src/components/scene.js
+++ src/components/scene.js
@@ -3,7 +3,7 @@ import { Color, Scene } from 'https://unpkg.com/three@0.127.0/build/three.module
 function createScene() {
   const scene = new Scene();
 
-  scene.background = new Color('skyblue');
+  scene.background = new Color('#ccb8d6');
 
   return scene;
 }
